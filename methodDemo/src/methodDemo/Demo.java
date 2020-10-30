38d66b0416d90187f1a790d267b0056e6156cef6
rdharmarajula <rajesh.dharmarajula@gmail.com>
2020-10-30 12:38:29 +0530

    test git from eclipse

---
+++ methodDemo/src/methodDemo/Demo.java
@@ -0,0 +1,9 @@
+package methodDemo;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		System.out.println("this is to test git from eclipse");
+	}
+
+}
---
+++ methodDemo/src/module-info.java
@@ -0,0 +1,2 @@
+module methodDemo {
+}
