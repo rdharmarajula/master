3e9a50d86980df58acbb04aa67e57db599d11b02
rdharmarajula <rajesh.dharmarajula@gmail.com>
2020-10-30 12:24:30 +0530

    java practise

---
+++ prasanna/src/test/Add.java
@@ -0,0 +1,14 @@
+package test;
+public class Add
+{
+public static void main(String[] args)
+{
+	int a=10;
+	a*=2;
+	int b=20;
+	//b=50;
+	int c=a+b;
+	System.out.println("Addition of 2 numbers is "+a+"+"+b+"="+c);
+	System.out.printf("%d+%d=%d",a,b,c);
+}
+}
